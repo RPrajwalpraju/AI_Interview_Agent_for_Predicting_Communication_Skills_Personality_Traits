46bf39dba8590ff2994782f2ea1959fa93f7d4d2
RPrajwalpraju <prajwalpraju2222@gmail.com>
2026-05-06 21:21:38 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,14 @@
+import multiprocessing
+from subprocess import Popen,PIPE
+import subprocess
+def worker(file):
+    print('# your subprocess code')
+    subprocess.Popen(['python', 'video_interview.py'])
+    subprocess.Popen(['python', 'quizstar.py'])
+
+
+if __name__ == '__main__':
+    files = ["video_interview.py"]
+    for i in files:
+        p = multiprocessing.Process(target=worker, args=(i,))
+        p.start()
